1875492c4c09f3dca781c8ab1b86682fa2e2c187
clyde <maxhaku.uni@gmail.com>
2024-11-30 19:14:35 +0100

    fixed bug in update event api

--- src/api/events/update-event.ts
+++ src/api/events/update-event.ts
@@ -1,12 +1,13 @@
 import { axios } from "@/providers";
 import { PatchEventRequest, PatchEventResponse } from "@/types";
 
-export async function patchEvent(
-  eventId: string,
-  eventData: PatchEventRequest
-) {
+interface EventData extends PatchEventRequest {
+  eventId: string;
+}
+
+export async function patchEvent(eventData: EventData) {
   return axios
-    .patch<PatchEventResponse>(`/events/${eventId}`, { ...eventData })
+    .patch<PatchEventResponse>(`/events/${eventData.eventId}`, { ...eventData })
     .then((res) => {
       return res.data;
     });
